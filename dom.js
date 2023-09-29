caf1c34b34737cbf3c0f106a63e284be66deaf6b
Garvit Gupta <146014535+garvit1312@users.noreply.github.com>
2023-09-29 17:45:50 +0530

    Add files via upload
    queryseletor

--- dom.js
+++ dom.js
@@ -35,14 +35,39 @@
 //}
 
 //GETELEMENTSBYCLASSNAMES//
-var li=document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].textContent ='hello 2';
-li[1].style.fontWeight ='bold';
-li[1].style.backgroundColor ='red';
+//var li=document.getElementsByTagName('li');
+//console.log(li);
+//console.log(li[1]);
+//li[1].textContent ='hello 2';
+//li[1].style.fontWeight ='bold';
+//li[1].style.backgroundColor ='red';
 //items.style.backgroundColor ='#f4f4f4';
-for(var i=0;i<li.length;i++){
-li[i].style.backgroundColor ='#f4f4f4';
+//for(var i=0;i<li.length;i++){
+//li[i].style.backgroundColor ='#f4f4f4';
+//}
 
+//QUERYSELECTOR//
+//var header=document.querySelector('#main-header');
+//header.style.borderBottom='solid 4px #ccc';
+//var input=document.querySelector('input');
+//input.value='hellow world';
+//var submit=document.querySelector('input[type="submit"]');
+//submit.value="send";
+
+//var item=document.querySelector('.list-group-item');
+//item.style.color='red';
+//var lastItem=document.querySelector('.list-group-item:last-child');
+//lastItem.style.color='blue';
+//var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+//secondItem.style.color='coral';
+
+//QUERY SELECTORAlLL//
+var titles=document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent='hello';
+var odd=document.querySelectorAll('li:nth-child(odd)');
+var even=document.querySelectorAll('li:nth-child(even)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='#f4f4f4';
+    even[i].style.backgroundColor='#ccc';
 }
