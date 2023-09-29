461caa65697fc6ec54defc6b13c6da7292389bd1
Garvit Gupta <146014535+garvit1312@users.noreply.github.com>
2023-09-29 15:08:43 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-//Examine the document object
+//Examine the document object//
 //console.dir(document);
 //console.log(document.domain);
 //console.log(document.URL);
@@ -11,13 +11,26 @@
 //console.log(document.forms);
 //console.log(document.links);
 
-//GetElementById
+//GetElementById //
 //console.log(document.getElementById('header-title'));
-var headerTitle =document.getElementById('header-title');
-var header =document.getElementById('main-header');
+//var headerTitle =document.getElementById('header-title');
+//var header =document.getElementById('main-header');
 //console.log(headerTitle);
 //headerTitle.textContent='hello';
 //headerTitle.innerText="good";
 //console.log(headerTitle.innerText);
 //headerTitle.innerHTML ='<h3>hello</h3>';
-header.style.borderBottom ='solid 3px #000';
+//header.style.borderBottom ='solid 3px #000';
+
+//GETELEMENTSBYCLASSNAMES//
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent ='hello 2';
+items[1].style.fontWeight ='bold';
+items[1].style.backgroundColor ='red';
+//items.style.backgroundColor ='#f4f4f4';
+for(var i=0;i<items.length;i++){
+items[i].style.backgroundColor ='#f4f4f4';
+
+}
