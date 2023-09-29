46fb2cf28d5da08e32011736587b733aec7a7968
Garvit Gupta <146014535+garvit1312@users.noreply.github.com>
2023-09-29 14:37:10 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,23 @@
+//Examine the document object
+//console.dir(document);
+//console.log(document.domain);
+//console.log(document.URL);
+//console.log(document.title);
+//console.log(document.doctype);
+//console.log(document.body);
+//console.log(document.all);
+//console.log(document.all[10]);
+//document.all[10].textContent="hello";
+//console.log(document.forms);
+//console.log(document.links);
+
+//GetElementById
+//console.log(document.getElementById('header-title'));
+var headerTitle =document.getElementById('header-title');
+var header =document.getElementById('main-header');
+//console.log(headerTitle);
+//headerTitle.textContent='hello';
+//headerTitle.innerText="good";
+//console.log(headerTitle.innerText);
+//headerTitle.innerHTML ='<h3>hello</h3>';
+header.style.borderBottom ='solid 3px #000';
